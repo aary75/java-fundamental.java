862ec961b0d5c0cf6d41708b12bf1aa9d7227243
Aaryan <aaryankgarg@gmail.com>
2025-02-25 14:14:31 +0530

    new

--- FirstJavaProgrammer.java
+++ FirstJavaProgrammer.java
@@ -1,5 +1,5 @@
-public class FirstjavaProgrammer{
+public class FirstJavaProgrammer{
    public static void main(String[] args){
-         System.out.print("Capgemini's training started");
+         System.out.print("BridgeLabz's training started");
 }}
 
