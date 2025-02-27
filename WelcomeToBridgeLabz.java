3c2866adfaa9d320cfd035d401bc0de9270c9f87
Aaryan <aaryankgarg@gmail.com>
2025-02-27 11:33:50 +0530

    Aaryan stats:Printing tbe message(Welcome to Bridgelabz)

---
+++ WelcomeToBridgeLabz.java
@@ -0,0 +1,4 @@
+public class WelcomeToBridgeLabz{
+     public static void main(String[] args){
+          System.out.print("Welcome to BridgeLabz");  // Printing the message "Welcome to BridgeLabz"
+}}
